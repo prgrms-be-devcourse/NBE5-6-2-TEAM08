b794adb8d20917f2bf2a2a1f7380b01fc504e5de
LightandSaltt <qkrtmd5@naver.com>
2025-05-14 17:02:48 +0900

    :fire: refactor - kakao api 관련 파일 삭제(미구현) feat : 코스 저장 관련 작업 - CourseService : 코스 저장 로직 - CourseRepository : save 추가 - CourseController : 주석 및 등록 post 매핑 추가 chore - make_mycourses.html 수정 - formmethod post 설정 / formaction make_mycourses 매핑

--- src/main/java/com/grepp/team08/app/model/course/service/CourseService.java
+++ src/main/java/com/grepp/team08/app/model/course/service/CourseService.java
@@ -1,5 +1,30 @@
 package com.grepp.team08.app.model.course.service;
 
+import com.grepp.team08.app.model.course.dto.CourseDto;
+import com.grepp.team08.app.model.course.entity.Course;
+import com.grepp.team08.app.model.course.repository.CourseRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class CourseService {
 
+    private final CourseRepository courseRepository;
+
+    @Autowired
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public void registCourses(CourseDto dto) {
+        try {
+            Course course = new Course();
+            course.setTitle("nana");  // 확정 아님
+            courseRepository.save(course);  // 저장
+        } catch (Exception e) {
+            throw new RuntimeException("코스 저장 중 오류 발생", e);
+        }
+    }
 }
