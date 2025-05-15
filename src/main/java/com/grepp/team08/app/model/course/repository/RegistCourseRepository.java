b8e214cb9ca2c6aac40d1acffaf62b1b23090098
LightandSaltt <qkrtmd5@naver.com>
2025-05-15 10:08:02 +0900

    feat - 코스 등록 DTO 추가      - make_mycourses 버튼 form 수정      - CourseService save 로직 수정      - RegistCourseRepository 추가      - CourseRepository 내 save 삭제

--- src/main/java/com/grepp/team08/app/model/course/repository/RegistCourseRepository.java
+++ src/main/java/com/grepp/team08/app/model/course/repository/RegistCourseRepository.java
@@ -1,14 +1,10 @@
 package com.grepp.team08.app.model.course.repository;
 
 import com.grepp.team08.app.model.course.entity.Course;
-import com.grepp.team08.app.model.course.entity.RecommendCourse;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends JpaRepository<RecommendCourse,Long> {
+public interface RegistCourseRepository extends JpaRepository<Course, Long> {
 
-  List<RecommendCourse> findTop4ByOrderByCreatedAtDesc();
-  
 }
