63ce8ca66b64e720a4e007b0bdf1c9b7fb5e6dc5
qkqehenr7 <qkqehenr37@gmail.com>
2025-06-11 02:57:49 +0900

    refactor : RecommendCourse 저장 시 Setter -> Builder 로 변경

--- src/main/java/com/grepp/team08/app/model/course/entity/RecommendCourse.java
+++ src/main/java/com/grepp/team08/app/model/course/entity/RecommendCourse.java
@@ -3,14 +3,20 @@ package com.grepp.team08.app.model.course.entity;
 import com.grepp.team08.app.model.member.entity.Member;
 import com.grepp.team08.infra.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Getter
-@Setter
 @ToString
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class RecommendCourse extends BaseEntity {
 
     @Id
